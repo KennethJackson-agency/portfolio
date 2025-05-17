515d2da49758d461e5f120bb1324c354eac8fca6
rafael cahya <cahyaputraugira95@gmail.com>
2025-05-17 16:36:23 +0700

    sitemap

---
+++ src/app/sitemap/route.js
@@ -0,0 +1,33 @@
+import { fetchAllProjects } from "@/lib/contentful";
+
+export async function GET() {
+	const baseUrl = 'https://kjagency.id';
+	const projects = await fetchAllProjects();
+
+	const urls = projects.map((project) => {
+		return `
+      <url>
+        <loc>${baseUrl}/projects/${project.fields.slug}</loc>
+        <lastmod>${new Date(project.sys.updatedAt).toISOString()}</lastmod>
+      </url>
+    `;
+	});
+
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    >
+      <url>
+        <loc>${baseUrl}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+      ${urls.join("")}
+    </urlset>
+  `;
+
+	return new Response(sitemap, {
+		headers: {
+			"Content-Type": "application/xml",
+		},
+	});
+}
